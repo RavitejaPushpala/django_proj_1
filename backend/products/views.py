c0e8f452592290eada2a7ae80a18c11bed4494d4
Raviteja <raviteja.pushpala@sojern.com>
2024-07-27 23:28:36 +0530

    added equivalent function based views for create,list,retrieve api class views

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 class ProductListCreateApiView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -22,5 +25,28 @@ class ProductDetailApiView(generics.RetrieveAPIView):
 product_detail_view = ProductDetailApiView.as_view()
 
 class ProductListApiView(generics.ListAPIView):
+    # not needed because it is covered in the ProductListCreateApiView
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    if request.method == "GET":
+        if pk is None:
+            queryset = Product.objects.all()
+            serializer = ProductSerializer(queryset, many=True)
+            return Response(serializer.data)
+        queryset = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(queryset)
+        return Response(serializer.data)
+    else:
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content") or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        else:
+            return Response({"invalid":"not a valid data"})
